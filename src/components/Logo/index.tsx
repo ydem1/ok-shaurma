313d6b0ade77beb44c71ee9144c1b6d241ffe226
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-14 16:44:53 +0300

    fix

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,27 +1,11 @@
-import { FC } from "react";
 import { Link } from "react-router-dom";
-import cn from "classnames";
 import { PATHNAMES } from "src/constants/routes";
 import logo from "src/assets/images/logo.png";
 
 const LOGO_IMAGE_ALT_TEXT = "ОК Шаурма";
 
-interface Props {
-  className?: string;
-  isSmall?: boolean;
-}
-
-export const Logo: FC<Props> = ({ className, isSmall }) => (
-  <Link
-    className={cn(className, {
-      "bg-black-primaty flex w-max items-center rounded-xl": !isSmall,
-    })}
-    to={PATHNAMES.HOME}
-  >
+export const Logo = () => (
+  <Link to={PATHNAMES.HOME}>
     <img className="w-20" src={logo} alt={LOGO_IMAGE_ALT_TEXT} />
-
-    {!isSmall && (
-      <span className="pr-4 text-3xl uppercase text-white-base">Шаурма</span>
-    )}
   </Link>
 );

a6fa0f8bae24a2d5d00864b7a381934da62940a6
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-14 16:41:06 +0300

    add meta

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { SocialLinks } from "./SocialLinks";
 
 export const Header = () => (
   <header className="shadow-custom bg-white-base">
-    <div className="container flex h-24 items-center justify-between xl:grid xl:grid-cols-3">
+    <div className="container flex items-center justify-between py-2 xl:grid xl:grid-cols-3">
       <CompanyInfo className="hidden xl:flex" />
 
       <div className="flex xl:justify-center">

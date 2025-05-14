cbd71e96b9b17f3f4072849d8e4415537c7258eb
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-14 14:16:51 +0300

    add adaptive for footer

--- src/components/Footer/WorkSchedule.tsx
+++ src/components/Footer/WorkSchedule.tsx
@@ -1,9 +1,11 @@
 import { Phone } from "../Phone";
 
 export const WorkSchedule = () => (
-  <div className="flex max-w-52 flex-col gap-1">
+  <div className="flex xl:max-w-52 flex-col gap-1">
     <Phone className="text-xl" />
+
     <span className="text-xs">щодня з 10:00 до 22:00</span>
+
     <span className="text-gray-dark mt-2 text-sm">
       м. Хмельницький, вул. Проскурівська, 70а, Січових Стрільців 6
     </span>

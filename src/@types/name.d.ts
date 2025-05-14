c89d193b30db20353cf7d9182f29a971b43a71c7
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-14 13:38:03 +0300

    fix

--- src/@types/name.d.ts
+++ src/@types/name.d.ts
@@ -2,5 +2,4 @@
 
 export type IconName =
 	| "instagram"
-	| "react"
 	| "tik-tok";
--- src/components/Footer/constants.ts
+++ src/components/Footer/constants.ts
@@ -4,26 +4,26 @@ export const SOCIAL_MEDIA_LINKS: ILink[] = [
   {
     id: 1,
     href: "https://github.com/ydem1",
-    icon: "react",
+    icon: "instagram",
   },
   {
     id: 2,
     href: "https://www.linkedin.com/in/vadym-bavorovskiy-146b19280/",
-    icon: "react",
+    icon: "instagram",
   },
   {
     id: 3,
     href: "https://t.me/vadymb14",
-    icon: "react",
+    icon: "instagram",
   },
   {
     id: 4,
     href: "mailto:vadymbavorovskyi@gmail.com",
-    icon: "react",
+    icon: "instagram",
   },
   {
     id: 5,
     href: "https://www.instagram.com/_ydem_/",
-    icon: "react",
+    icon: "instagram",
   },
 ];

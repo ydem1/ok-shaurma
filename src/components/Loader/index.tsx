c779007ddf7f1c9dcb54f0c3b244d900cb7d03ab
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-17 20:00:48 +0300

    add loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import cn from "classnames";
+import { Sizes } from "src/@types/sizes";
+import { LOADER_SIZES } from "./constants";
+
+interface Props {
+  size?: Sizes;
+  className?: string;
+}
+
+export const Loader: FC<Props> = ({ size = Sizes.XS, className }) => (
+  <div
+    className={cn(
+      "border-gray-base border-t-black-primaty mx-auto animate-spin rounded-full border-2",
+      LOADER_SIZES[size],
+      className
+    )}
+  />
+);
+
+// black: {
+//         primaty: "#151616",
+//       },
+
+//          gray: {
+//         base: "#F2F2F2",
+//         dark: "#6E6F84",
+//       },
--- src/page-components/home/Menu/index.tsx
+++ src/page-components/home/Menu/index.tsx
@@ -1,20 +1,24 @@
 import React, { FC } from "react";
+import { Loader } from "src/components/Loader";
 import { useGetMenuQuery } from "src/store/menu";
+import { Sizes } from "src/@types/sizes";
 import { ProductItem } from "./ProductItem";
 
 export const Menu: FC = () => {
-  const { data } = useGetMenuQuery({});
-
-  console.log(data)
+  const { data: menu, isFetching: isLoadingMenu } = useGetMenuQuery({});
 
   return (
     <section className="container">
       <h2 className="text-2xl font-bold">Шаурма</h2>
 
       <div className="mt-9 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {data?.map((item) => (
-          <ProductItem key={item._id} {...item} />
-        ))}
+        {isLoadingMenu && (
+          <div className="col-span-full flex h-[50vh] items-center justify-center">
+            <Loader size={Sizes.XXL} />
+          </div>
+        )}
+
+        {menu && menu.map((item) => <ProductItem key={item._id} {...item} />)}
       </div>
     </section>
   );

0edd64bbf427ced6391c330ac311df27c077c374
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-19 13:28:58 +0300

    add temp menu

--- src/page-components/home/Menu/ProductItem.tsx
+++ src/page-components/home/Menu/ProductItem.tsx
@@ -1,28 +1,35 @@
 import React, { FC } from "react";
-import imageTemp from "./temp.jpg";
 
-const temp_product = {
-  name: "М'ясна",
-  image: imageTemp,
-  price: 200,
-  weight: 320,
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi vo",
-};
+interface Props {
+  _id: string;
+  name: string;
+  image: string;
+  price: number;
+  weight: number;
+  description: string;
+}
 
-export const ProductItem: FC = () => {
-  const { name, image, price, weight, description } = temp_product;
-
+export const ProductItem: FC<Props> = ({
+  name,
+  image,
+  price,
+  weight,
+  description,
+}) => {
   return (
-    <article className="shadow-custom bg-white-base px-4 pb-9 pt-4 rounded-2xl">
-      <div>
-        <img className="w-full rounded-xl" src={image} alt={name} />
+    <article className="rounded-2xl bg-white-base px-4 pb-9 pt-4 shadow-custom">
+      <div className="h-60">
+        <img
+          className="h-full w-full rounded-xl object-cover"
+          src={image}
+          alt={name}
+        />
       </div>
 
       <div className="mt-4 flex flex-col gap-2.5">
-        <span className="text-gray-dark text-xs">{`${weight}г.`}</span>
+        <span className="text-xs text-gray-dark">{`${weight}г.`}</span>
         <span className="text-lg font-bold">{name}</span>
-        <span className="text-gray-dark text-xs">{description}</span>
+        <span className="text-xs text-gray-dark">{description}</span>
 
         <div>
           <span className="text-2xl font-bold">{price}&#8372;</span>

224246d85853e21a62cfbe88ca3236b8d38f73a5
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-22 22:21:32 +0300

    add load image for menu list in admin

--- src/page-components/admin/ListMenu/index.tsx
+++ src/page-components/admin/ListMenu/index.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import cn from "classnames";
 import imageTemp from "src/page-components/home/Menu/temp.jpg";
 import { Button } from "src/components/Button";
 import { Loader } from "src/components/Loader";
 import { useDeleteMenuItemMutation, useGetMenuQuery } from "src/store/menu";
+import { getImageUrl } from "src/utils/getImageUrl";
 import { NotificationService } from "src/helpers/notifications";
 import { IMenuItem } from "src/@types/menu-item";
 import { Sizes } from "src/@types/sizes";
@@ -51,47 +52,59 @@ export const ListMenu: FC<Props> = ({ className, handleEditItem }) => {
               <div>Ціна</div>
               <div className="text-right">Керування</div>
             </li>
-            {menu.map(({ _id, name, price, weight, description, image }) => (
-              <li
-                key={_id}
-                className="grid grid-cols-[80px_2fr_2fr_4fr_auto_1fr] items-center gap-4 border-b p-5 last:border-none"
-              >
-                <div className="h-20 w-20">
-                  <img
-                    className="h-full w-full rounded-xl object-cover"
-                    src={imageTemp}
-                    alt={name}
-                  />
-                </div>
-                <div>{weight}г</div>
-                <div>{name}</div>
-                <div>{description}</div>
-                <div>{price}&#8372;</div>
+            {menu.map(({ _id, name, price, weight, description, image }) => {
+              // eslint-disable-next-line react-hooks/rules-of-hooks
+              const [previewSrc, setPreviewSrc] = useState<string>(
+                getImageUrl(image)
+              );
 
-                <div className="flex items-center justify-end gap-3">
-                  <Button
-                    onClick={() =>
-                      handleEdit({
-                        _id,
-                        name,
-                        price,
-                        weight,
-                        description,
-                        image,
-                      })
-                    }
-                  >
-                    ✏️
-                  </Button>
-                  <Button
-                    onClick={() => handleDelete(_id)}
-                    isDisabled={isLoadingDelete}
-                  >
-                    ❌
-                  </Button>
-                </div>
-              </li>
-            ))}
+              const handleError = () => {
+                setPreviewSrc(imageTemp);
+              };
+
+              return (
+                <li
+                  key={_id}
+                  className="grid grid-cols-[80px_2fr_2fr_4fr_auto_1fr] items-center gap-4 border-b p-5 last:border-none"
+                >
+                  <div className="h-20 w-20">
+                    <img
+                      className="h-full w-full rounded-xl object-cover"
+                      src={previewSrc}
+                      alt={name}
+                      onError={handleError}
+                    />
+                  </div>
+                  <div>{weight}г</div>
+                  <div>{name}</div>
+                  <div>{description}</div>
+                  <div>{price}&#8372;</div>
+
+                  <div className="flex items-center justify-end gap-3">
+                    <Button
+                      onClick={() =>
+                        handleEdit({
+                          _id,
+                          name,
+                          price,
+                          weight,
+                          description,
+                          image,
+                        })
+                      }
+                    >
+                      ✏️
+                    </Button>
+                    <Button
+                      onClick={() => handleDelete(_id)}
+                      isDisabled={isLoadingDelete}
+                    >
+                      ❌
+                    </Button>
+                  </div>
+                </li>
+              );
+            })}
           </>
         )}
       </ul>

65c000fea23182c607369ab30b9dd28ffe315589
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-22 22:24:02 +0300

    fix

--- src/page-components/admin/ListMenu/index.tsx
+++ src/page-components/admin/ListMenu/index.tsx
@@ -1,6 +1,5 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import cn from "classnames";
-import imageTemp from "src/page-components/home/Menu/temp.jpg";
 import { Button } from "src/components/Button";
 import { Loader } from "src/components/Loader";
 import { useDeleteMenuItemMutation, useGetMenuQuery } from "src/store/menu";
@@ -52,59 +51,47 @@ export const ListMenu: FC<Props> = ({ className, handleEditItem }) => {
               <div>Ціна</div>
               <div className="text-right">Керування</div>
             </li>
-            {menu.map(({ _id, name, price, weight, description, image }) => {
-              // eslint-disable-next-line react-hooks/rules-of-hooks
-              const [previewSrc, setPreviewSrc] = useState<string>(
-                getImageUrl(image)
-              );
+            {menu.map(({ _id, name, price, weight, description, image }) => (
+              <li
+                key={_id}
+                className="grid grid-cols-[80px_2fr_2fr_4fr_auto_1fr] items-center gap-4 border-b p-5 last:border-none"
+              >
+                <div className="h-20 w-20">
+                  <img
+                    className="h-full w-full rounded-xl object-cover"
+                    src={getImageUrl(image)}
+                    alt={name}
+                  />
+                </div>
+                <div>{weight}г</div>
+                <div>{name}</div>
+                <div>{description}</div>
+                <div>{price}&#8372;</div>
 
-              const handleError = () => {
-                setPreviewSrc(imageTemp);
-              };
-
-              return (
-                <li
-                  key={_id}
-                  className="grid grid-cols-[80px_2fr_2fr_4fr_auto_1fr] items-center gap-4 border-b p-5 last:border-none"
-                >
-                  <div className="h-20 w-20">
-                    <img
-                      className="h-full w-full rounded-xl object-cover"
-                      src={previewSrc}
-                      alt={name}
-                      onError={handleError}
-                    />
-                  </div>
-                  <div>{weight}г</div>
-                  <div>{name}</div>
-                  <div>{description}</div>
-                  <div>{price}&#8372;</div>
-
-                  <div className="flex items-center justify-end gap-3">
-                    <Button
-                      onClick={() =>
-                        handleEdit({
-                          _id,
-                          name,
-                          price,
-                          weight,
-                          description,
-                          image,
-                        })
-                      }
-                    >
-                      ✏️
-                    </Button>
-                    <Button
-                      onClick={() => handleDelete(_id)}
-                      isDisabled={isLoadingDelete}
-                    >
-                      ❌
-                    </Button>
-                  </div>
-                </li>
-              );
-            })}
+                <div className="flex items-center justify-end gap-3">
+                  <Button
+                    onClick={() =>
+                      handleEdit({
+                        _id,
+                        name,
+                        price,
+                        weight,
+                        description,
+                        image,
+                      })
+                    }
+                  >
+                    ✏️
+                  </Button>
+                  <Button
+                    onClick={() => handleDelete(_id)}
+                    isDisabled={isLoadingDelete}
+                  >
+                    ❌
+                  </Button>
+                </div>
+              </li>
+            ))}
           </>
         )}
       </ul>

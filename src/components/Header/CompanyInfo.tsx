ef7fdb692e52ac4e8c384b39f20b2709a0272292
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-24 15:24:42 +0300

    hidden city

--- src/components/Header/CompanyInfo.tsx
+++ src/components/Header/CompanyInfo.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const CompanyInfo: FC<Props> = ({ className, contact }) => (
   <div className={cn("flex items-center gap-7", className)}>
-    <span className="hidden text-base font-bold xl:block">Хмельницький</span>
+    {/* <span className="hidden text-base font-bold xl:block">Хмельницький</span> */}
     {contact._id && <Phone contact={contact} />}
   </div>
 );

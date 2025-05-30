3f09ddddef3719d221ac1ba6ae78da75d01faf17
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-05-30 15:02:05 +0300

    add seo

--- src/components/Footer/WorkSchedule.tsx
+++ src/components/Footer/WorkSchedule.tsx
@@ -7,9 +7,9 @@ export const WorkSchedule: FC<IContact> = ({
   address,
   ...contact
 }) => (
-  <div className="flex flex-col gap-3 xl:max-w-80">
+  <p className="flex flex-col gap-3 xl:max-w-80">
     <Phone className="text-xl" contact={{ schedule, address, ...contact }} />
     <span className="text-xs">{schedule}</span>
     <span className="mt-2 text-sm text-gray-dark">{address}</span>
-  </div>
+  </p>
 );
